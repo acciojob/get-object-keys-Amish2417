6fbc1ff6f08f60f4668dd87f4fef9a4fa49b91d6
ANISH KUMAR <70019437+Amish2417@users.noreply.github.com>
2024-05-14 16:43:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,6 +3,11 @@
 let student = {
 	name: "Raul",
 }
-Object.prototype.getKeys = function () {
-	return Object.keys(this);
+let arr = [];
+Object.prototype.getKeys = () => {
+	// return Object.keys(this);
+	for(let t in student){
+		arr.push(t);
+	}
+	return arr;
 }
