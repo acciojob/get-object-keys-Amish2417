c9cf7c9d8042cde370e706c7abf96f960342086f
ANISH KUMAR <70019437+Amish2417@users.noreply.github.com>
2024-05-14 12:06:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+ let student = {
+	 name : 'anish',
+	 
+ }
+let arr = [];
+student.prototype.getkeys = () =>{
+	for(let t in obj){
+		arr.push(t);
+	}
+	console.log(arr);
+	return arr;
+}
+
